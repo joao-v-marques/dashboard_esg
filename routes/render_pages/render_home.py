b59da94c9ccf7f82540ca2058bb36f3ec5dcac4f
joao-v-marques <joaovmarques2006@outlook.com>
2026-08-06 14:25:21 -0300

    feat: ajustado os token_required's nas rotas de renderização de páginas

--- routes/render_pages/render_home.py
+++ routes/render_pages/render_home.py
@@ -3,7 +3,7 @@ from middlewares.jwt_middleware import token_required
 
 bp_render_home = Blueprint("bp_render_home", __name__)
 
-@token_required
 @bp_render_home.route("/home", methods=['GET'])
+@token_required
 def render_home():
     return render_template("home.html")
--- routes/render_pages/render_login.py
+++ routes/render_pages/render_login.py
@@ -1,9 +1,7 @@
 from flask import Blueprint, render_template
-from middlewares.jwt_middleware import token_required
 
 bp_render_login = Blueprint("bp_render_login", __name__)
 
-@token_required
 @bp_render_login.route("/login", methods=['GET'])
 def render_login():
     return render_template("login.html")
