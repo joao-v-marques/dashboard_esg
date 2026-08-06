827230ea17f688a0cd0e33fbbfcadb639971e448
joao-v-marques <joaovmarques2006@outlook.com>
2026-08-06 14:27:08 -0300

    feat: adicionado os enpoints de autenticação nas render_blueprints

--- configs.py
+++ configs.py
@@ -1,6 +1,8 @@
 from routes.render_pages.render_home import bp_render_home
 from routes.render_pages.render_login import bp_render_login
 
+from routes.auth_controller import bp_auth
+
 PREFIX = "/dashboard-esg"
 
 def config_all(app):
@@ -9,3 +11,4 @@ def config_all(app):
 def config_bps(app):
     app.register_blueprint(bp_render_home, url_prefix=PREFIX)
     app.register_blueprint(bp_render_login, url_prefix=PREFIX)
+    app.register_blueprint(bp_auth, url_prefix=PREFIX)
